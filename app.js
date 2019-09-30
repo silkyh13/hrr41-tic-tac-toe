c1c53eb3618117e2e1839c0c4f58ae1322079068
“silkyh13” <silkyh13@gmail.com>
2019-09-30 19:51:43 -0400

    can toggle between X and O

--- app.js
+++ app.js
@@ -1,19 +1,38 @@
 var start = function () {
   var table = document.getElementById('mytab1');
   // console.log('array', array);
-  console.log(table.rows[0].cells[0]);
-  var r = 0;
-  var row, cell;
-  //go through each row
-  while (row = table.rows[r++]) {
-    var c = 0;
-    //go through each indiv box
-    while (cell = row.cells[c++]) {
+  // var individual
+  // console.log('first value', table.rows[0].cells[0].getElementsByClassName('square').id);
 
-
-      cell.innerHTML='X'; // do sth with cell
+  for (var i = 0; i < table.rows.length; i++) {
+    var number = 0;
+    for (var j = 0; j < table.rows[i].cells.length; j++) {
+      table.rows[i].cells[j].onclick = function () {
+        // if (number = 0)
+        // console.log('this', this.id);
+        if (number % 2 === 0) {
+          number++;
+          this.innerHTML = 'X';
+        } else if (number % 2 === 1) {
+          number++;
+          this.innerHTML = 'O';
+        }
+      };
     }
   }
+
+  // var r = 0;
+  // var row, cell;
+  // //go through each row
+  // while (row = table.rows[r++]) {
+  //   var c = 0;
+  //   //go through each indiv box
+  //   while (cell = row.cells[c++]) {
+
+
+  //     cell.innerHTML='X'; // do sth with cell
+  //   }
+  // }
 
   // for (var i = 0; i < table.length; i++) {
   //   console.log(i)
