a8e9ae279ebd9f5420bf8f7c35b8bf330bb431c9
“silkyh13” <silkyh13@gmail.com>
2019-09-30 23:55:40 -0400

    completed basic requirements

--- app.js
+++ app.js
@@ -1,11 +1,60 @@
-var start = function () {
-  var table = document.getElementById('mytab1');
+let isGameOver = false;
+let team;
+let table = document.getElementById('mytab1');
 
+var resetBoard = () => {
+  document.getElementById('button').addEventListener('click', () => {
+    restart();
+  });
+};
+resetBoard();
+//checking if there is a win
+var gameOver = function (table, i, j) {
+  var currentRowCell0 = getValAtLocation(table, i, 0);
+  var currentRowCell1 = getValAtLocation(table, i, 1);
+  var currentRowCell2 = getValAtLocation(table, i, 2);
+  var otherRowCurrentCell0 = getValAtLocation(table, 0, j);
+  var otherRowCurrentCell1 = getValAtLocation(table, 1, j);
+  var otherRowCurrentCell2 = getValAtLocation(table, 2, j);
+  var row0column0 = getValAtLocation(table, 0, 0);
+  var row1column1 = getValAtLocation(table, 1, 1);
+  var row2column2 = getValAtLocation(table, 2, 2);
+  var row0column2 = getValAtLocation(table, 0, 2);
+  var row2column0 = getValAtLocation(table, 2, 0);
+
+  if (currentRowCell0 === currentRowCell1 && currentRowCell1 === currentRowCell2) {
+    return currentRowCell0;
+  }
+  if (otherRowCurrentCell0 === otherRowCurrentCell1 && otherRowCurrentCell1 === otherRowCurrentCell2) {
+    return otherRowCurrentCell0;
+  }
+  if (row0column0 === row1column1 && row1column1 === row2column2) {
+
+    return row0column0;
+  }
+  if (row0column2 === row1column1 && row1column1 === row2column0) {
+
+    return row0column2;
+  }
+
+  return false;
+};
+
+//is it X or O
+const getValAtLocation = (table, i, j) => {
+  return table.rows[i].cells[j].innerHTML;
+};
+
+//give it an X or O
+const setUpClickEvent = () => {
   for (let i = 0; i < table.rows.length; i++) {
     var number = 0;
     for (let j = 0; j < table.rows[i].cells.length; j++) {
-
       table.rows[i].cells[j].onclick = function () {
+        //get rid of start once you begin
+        // if (getValAtLocation(table, 1, 1) === 'start') {
+        //   table.rows[1].cells[1].innerHTML = '';
+        // }
         if (number % 2 === 0) {
           number++;
           this.innerHTML = 'X';
@@ -15,51 +64,59 @@ var start = function () {
           this.innerHTML = 'O';
           console.log('table', table.rows[i].cells[j]);
         }
-        var currentRowCell0 = table.rows[i].cells[0].innerHTML;
-        var currentRowCell1 = table.rows[i].cells[1].innerHTML;
-        var currentRowCell2 = table.rows[i].cells[2].innerHTML;
-        var otherRowCurrentCell0 = table.rows[0].cells[j].innerHTML;
-        var otherRowCurrentCell1 = table.rows[1].cells[j].innerHTML;
-        var otherRowCurrentCell2 = table.rows[2].cells[j].innerHTML;
-        var row0column0 = table.rows[0].cells[0].innerHTML;
-        var row1column1 = table.rows[1].cells[1].innerHTML;
-        var row2column2 = table.rows[2].cells[2].innerHTML;
-        var row0column2 = table.rows[0].cells[2].innerHTML;
-        var row2column0 = table.rows[2].cells[0].innerHTML;
-        var gameOver = function () {
-          if (currentRowCell0 === currentRowCell1) {
-            if (currentRowCell1 === currentRowCell2) {
-              console.log('you win for row');
-            }
-          }
-          if (otherRowCurrentCell0 === otherRowCurrentCell1) {
-            if (otherRowCurrentCell1 === otherRowCurrentCell2) {
-              console.log('you win for column');
-            }
-          }
-          if (row0column0 === row1column1) {
-            if (row1column1 === row2column2) {
-              console.log('you win for right diagonal');
-            }
-          }
-          if (row0column2 === row1column1) {
-            if (row1column1 === row2column0) {
-              console.log('you win for left diagonal');
-            }
-          }
-        };
-        gameOver();
+        //changing the value of isGameOver
+        isGameOver = gameOver(table, i, j);
+        if (isGameOver) {
+          alert(isGameOver + ' wins');
+        }
+        game();//looks at value of isGameOver
       };
-
-
     }
-    // console.log('table', table.rows[0]);
   }
-  //look through table.rows[i]
-  // for (let i = 0; i < table.rows[0].length; i++) {
-  //   if ()
-  // }
-
 };
 
-start();
+var restart = function () {
+  // initializing the values
+  isGameOver = false;
+  setUpClickEvent();//clicky part
+  for (let i = 0; i < table.rows.length; i++) {
+    for (let j = 0; j < table.rows[i].cells.length; j++) {
+      if (i === j && i === 1) {
+        table.rows[i].cells[j].innerHTML = 'start';
+        continue;
+      }
+      table.rows[i].cells[j].innerHTML = '';
+    }
+  }
+
+};
+//write a function
+var isFull = () => {
+  for (let i = 0; i < table.rows.length; i++) {
+    for (let j = 0; j < table.rows[i].cells.length; j++) {
+      if (table.rows[i].cells[j].innerHTML === '' || table.rows[i].cells[j].innerHTML === 'start') { //it is empty, not full
+        return false;
+      }
+    }
+  }
+  return true;//everything is filled out
+};
+isFull();
+var game = function () {
+  console.log(isFull(), 'is it false?');
+  if (isFull() && !isGameOver) {
+    // console.log('tie')
+    alert('it\'s a tie');
+
+  } else if (isGameOver) {
+    // someone won so we are going to disable clicks & show the reset button
+    for (let i = 0; i < table.rows.length; i++) {
+      for (let j = 0; j < table.rows[i].cells.length; j++) {
+        table.rows[i].cells[j].onclick = null;
+      }
+    }
+  }
+};
+
+
+restart();
